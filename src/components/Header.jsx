16358bddde53b1d8c84653b006ffad9a3d89b8b6
lucianocabelloo <you@example.com>
2024-01-18 18:41:49 -0300

    Add Imgs in utils

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import headerIMG from "../assets/img1.jpg"
+import headerIMG from "../assets/prueba3.png"
 
 export const Header = () => {
   return (
@@ -9,7 +9,7 @@ export const Header = () => {
         <div className=' w-2/4 h-[500px]'>
           <img className='m-auto h-full' src={headerIMG} alt="Imagen de veterinaria revisando un cachorro" />
         </div>
-        <div className='flex flex-col gap-6 p-4 w-2/4'>
+        <div className='flex flex-col justify-center items-center gap-6 p-4 w-2/4'>
           <h1 className=' text-5xl w-[15ch] leading-[55px] font-bold'>VitalPet: Donde la Salud y la Felicidad de tu Mascota se Encuentran</h1>
           <p className='  text-gray-500 font-mono text-justify w-[50ch]'>¡Cuida su felicidad, confía en nuestra experiencia! En nuestra veterinaria, cada ronroneo y ladrido es una historia de bienestar. Porque amamos a tus mascotas tanto como vos.</p>
         </div>
