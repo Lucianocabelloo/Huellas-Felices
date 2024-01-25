24f9b9d0e9017c2f07cb60bd0fd1f5c8e920fea5
lucianocabelloo <you@example.com>
2024-01-25 18:44:26 -0300

    Add maquetado card and button add veterinary

--- src/components/VetsAdministrator.jsx
+++ src/components/VetsAdministrator.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import VetCard from './VetCard'
+import AddIcon from '@mui/icons-material/Add';
 
 const VetsAdministrator = () => {
   return (
-    <div>VetsForm</div>
+    <>
+    <h1 className=' text-5xl text-center py-5'>Veterinarios Disponibles</h1>
+    <section className='flex flex-wrap gap-11 items-center p-5'>
+
+    <div>
+      <VetCard/>
+    </div>
+
+    <div className=' p-20 border border-slate-300  bg-white hover:bg-[#bfc0c3]	flex justify-center items-center  max-w-[10%] rounded-lg cursor-pointer m-auto lg:m-0'>
+      <AddIcon sx={{ fontSize: 80 }}/>
+    </div>
+    </section>
+    </>
   )
 }
 
